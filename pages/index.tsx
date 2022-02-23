0e7d66ebfc1bfc2ec57898e7b2e41bb6ed5d7162
Jonathan Voss <jv96@mahurangi.school.nz>
2022-02-24 09:45:29 +1300

    Book layouts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,50 @@
-import type { NextPage } from 'next';
+import { Prisma } from '@prisma/client';
+import type { GetServerSideProps, NextPage } from 'next';
+import  prisma from '../prisma/database'
 
-const Home: NextPage = () => {
+interface Book {
+    id: number;
+    title: string;
+}
+interface StudentBook {
+    id: number;
+    book: Book;
+}
+interface Student {
+    id: number;
+    firstName: string;
+    lastName: string;
+    books: StudentBook[];
+}
+
+interface HomeProps {
+    student: Student
+}
+
+const Home: NextPage<HomeProps> = ({ student}) => {
     return (
-        <p>
-            please open a document. exmaple url:
-            <br />
-            <a href="/document/1">/document/1</a>
-        </p>
+        <div>
+            {
+                student.books.map(book => (
+                    <div key={book.id}>
+                        <h1>{book.book.title}</h1>
+                    </div>))
+            }
+        </div>
+
     );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const student = await prisma.student.findUnique({
+        where: {
+            id: 1,
+        },
+    })
+
+    return { props: {
+        student 
+    }};
 };
 
 export default Home;
