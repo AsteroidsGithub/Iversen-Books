291af32a407c583a297a3f7c79b5c7f48261f37d
Jonathan Voss <jv96@mahurangi.school.nz>
2022-05-12 10:14:15 +1200

    meta: switched react over to react-pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import '../styles/globals.css'
 import React from "react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default ({ Component, pageProps }: AppProps) => {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <meta name="application-name" content="Quick60" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta
+                    name="apple-mobile-web-app-status-bar-style"
+                    content="default"
+                />
+                <meta name="apple-mobile-web-app-title" content="Quick60" />
+                <meta name="description" content="Best PWA App in the world" />
+                <meta name="format-detection" content="telephone=no" />
+                <meta name="mobile-web-app-capable" content="yes" />
+                <meta
+                    name="msapplication-config"
+                    content="/icons/browserconfig.xml"
+                />
+                <meta name="msapplication-TileColor" content="#2B5797" />
+                <meta name="msapplication-tap-highlight" content="no" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 };
 
