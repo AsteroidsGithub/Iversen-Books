2995e0ee66095cc78ff4562475844e8a85f27ffc
Jonathan Voss <jv96@mahurangi.school.nz>
2022-05-30 09:11:31 +1200

    bugs: fixed the viewport issue on Safari for iOS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,23 +9,19 @@ export default ({ Component, pageProps }: AppProps) => {
             <Head>
                 <meta name="application-name" content="Quick60" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
-                <meta
-                    name="apple-mobile-web-app-status-bar-style"
-                    content="default"
-                />
+                <meta name="apple-mobile-web-app-status-bar-style" content="default" />
                 <meta name="apple-mobile-web-app-title" content="Quick60" />
                 <meta name="description" content="Best PWA App in the world" />
                 <meta name="format-detection" content="telephone=no" />
                 <meta name="mobile-web-app-capable" content="yes" />
-                <meta
-                    name="msapplication-config"
-                    content="/icons/browserconfig.xml"
-                />
+                <meta name="msapplication-config" content="/icons/browserconfig.xml" />
                 <meta name="msapplication-TileColor" content="#2B5797" />
                 <meta name="msapplication-tap-highlight" content="no" />
                 <meta name="theme-color" content="#ffffff" />
             </Head>
-            <Component {...pageProps} />
+            <div className="absolute inset-0">
+                <Component {...pageProps} />
+            </div>
         </>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import type { NextPage } from 'next';
 
+import elenSpin from '../public/spin.gif';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
@@ -10,8 +11,8 @@ const Home: NextPage = () => {
     const router = useRouter();
 
     return (
-        <div className="flex h-screen w-screen items-center justify-center bg-blue-400">
-            <div className="flex flex-col items-center justify-center bg-white py-10 px-6">
+        <div className=" h-view bg-blue-400">
+            <div className="flex w-14 flex-col items-center justify-center bg-white py-10 px-6">
                 <h1 className="text-3xl text-black">
                     Welcome to <span className="font-semibold">Quick60</span>
                 </h1>
@@ -20,35 +21,55 @@ const Home: NextPage = () => {
                         email: '',
                         password: '',
                     }}
-                    onSubmit={(formData) => {
+                    onSubmit={(formData, { setSubmitting }) => {
                         axios
                             .post('/api/authUser', formData)
 
-                            .then(() => router.push('/protected'))
+                            .then(() => {
+                                router.push('/protected');
+                                setSubmitting(true);
+                            })
                             .catch((err) => {
                                 console.log(err);
                                 setFormError(err.response.data.message || 'Something went wrong');
+                                setSubmitting(false);
                             });
                     }}
                 >
-                    <Form className="flex w-full flex-col space-y-2">
-                        <p className="text-red-600">{formError}</p>
+                    {({ isSubmitting }) => (
+                        <Form className="flex w-full flex-col space-y-2">
+                            {!isSubmitting ? (
+                                <>
+                                    <p className="text-red-600">{formError}</p>
 
-                        <label className="mt-2 block text-sm font-bold">Email</label>
-                        <Field type="email" placeholder="Email" name="email" />
-                        <ErrorMessage name="email" component="div" className="text-red-500" />
+                                    <label className="mt-2 block text-sm font-bold">Email</label>
+                                    <Field type="email" placeholder="Email" name="email" />
+                                    <ErrorMessage
+                                        name="email"
+                                        component="div"
+                                        className="text-red-500"
+                                    />
 
-                        <label className="mt-2 block text-sm font-bold">Password</label>
-                        <Field type="password" placeholder="Password" name="password" />
-                        <ErrorMessage name="password" component="div" className="text-red-500" />
+                                    <label className="mt-2 block text-sm font-bold">Password</label>
+                                    <Field type="password" placeholder="Password" name="password" />
+                                    <ErrorMessage
+                                        name="password"
+                                        component="div"
+                                        className="text-red-500"
+                                    />
 
-                        <button
-                            type="submit"
-                            className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
-                        >
-                            Login
-                        </button>
-                    </Form>
+                                    <button
+                                        type="submit"
+                                        className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+                                    >
+                                        Login
+                                    </button>
+                                </>
+                            ) : (
+                                <img className="" src={elenSpin.src}></img>
+                            )}
+                        </Form>
+                    )}
                 </Formik>
             </div>
         </div>
