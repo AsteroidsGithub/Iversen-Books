57c50245cbe4c8cf6ad3bb74d373c4dd527f1df1
Jonathan Voss <jv96@mahurangi.school.nz>
2022-06-28 10:45:13 +1200

    pages: setup site to use modal controll for everything

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,6 +1,15 @@
+import useSharedState from '@Middleware/useSharedState';
+
 const BookCover: React.FC<{}> = ({}) => {
+  const { setActiveModal } = useSharedState();
+
   return (
-    <div className=" flex w-full flex-col items-center justify-center">
+    <div
+      className=" flex w-full flex-col items-center justify-center"
+      onClick={() => {
+        setActiveModal(<h1>f</h1>);
+      }}
+    >
       <img src="https://via.placeholder.com/128" alt="book cover" className="w-full" />
       <h2 className="text-center">Book cover Template</h2>
       <h3 className="text-center">
