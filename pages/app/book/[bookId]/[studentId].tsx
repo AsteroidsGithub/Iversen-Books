09b2c7146497ab9c4b99ef42d5392b16db1c51b3
Jonathan Voss <jv96@mahurangi.school.nz>
2022-07-07 09:28:44 +1200

    bugs: fix users not being pulled after refresh

--- pages/app/book/[bookId]/[studentId].tsx
+++ pages/app/book/[bookId]/[studentId].tsx
@@ -1,14 +1,20 @@
 import PillHeader from '@Components/PillHeader';
+import { GetServerSideProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { User } from '@Interfaces/users';
+import getUser from '@Utilities/getUser';
+import useSharedState from '@Middleware/useSharedState';
 
-const Post = () => {
-  const router = useRouter();
-  const { bookId, studentId } = router.query;
+const Post: NextPage<{ user: User }> = ({ user }) => {
+  const { bookId, studentId } = useRouter().query;
+  useSharedState().setUser(user);
 
   const test: string[] =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisi vel consectetur euismod, nisi nisl aliquet nisl, eget consectetur nisl nisi vel nisl.'.split(
       ' ',
     );
+
+  const testType: string[] = ['New Skill', 'PT SKill'];
 
   return (
     <>
@@ -17,7 +23,7 @@ const Post = () => {
         <h2>Pages 1 & 2</h2>
         <p>
           {test.map((word, i) => (
-            <span key={i} onClick={() => console.log(word)}>
+            <span key={i} onClick={() => console.log(word)} className="cursor-pointer">
               {word}{' '}
             </span>
           ))}
@@ -27,4 +33,5 @@ const Post = () => {
   );
 };
 
+export const getServerSideProps: GetServerSideProps = (context) => getUser(context);
 export default Post;
--- utilities/getUser.ts
+++ utilities/getUser.ts
@@ -5,6 +5,8 @@ import checkAuth from './checkAuth';
 export default async (context: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>) => {
   const user = await checkAuth(context.req.cookies.auth);
 
+  console.log('\n User Found \n');
+
   if (!user) {
     console.log('\nBlocked access to protected page\n');
     console.log(context.req.cookies);
