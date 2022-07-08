64ec2bcd9e5f681274df8e746a87f2347cdd75b4
Jonathan Voss <jv96@mahurangi.school.nz>
2022-07-08 14:06:29 +1200

    books: real data for Dempster

--- pages/app/book/[bookId]/[studentId].tsx
+++ pages/app/book/[bookId]/[studentId].tsx
@@ -8,26 +8,74 @@ import useSharedState from '@Middleware/useSharedState';
 const Post: NextPage<{ user: User }> = ({ user }) => {
   const { bookId, studentId } = useRouter().query;
   useSharedState().setUser(user);
+  // Pages > Lines
+  const bookRawData: String[][] = [
+    [
+      'What did you have for breakfast today? Did you have a bran muffin?',
+      'Did you have pancakes?',
+      'Did you have cornflakes?',
+      'Some people eat these things',
+      'for breakfast.',
+      'But did you know that sometimes',
+      'people eat and drink other things, too?',
+    ],
+    [
+      'Did you know that',
+      'sometimes people in Mexico',
+      'eat burritos for breakfast?',
+      'They cook meat with tomato and eggs. They grate some cheese.',
+      'They cut some lettuce.',
+      'They warm tortillas.',
+      'They put everything in a warm tortilla and eat it for breakfast.',
+    ],
+    [
+      'Did you know that',
+      'sometimes people in Korea',
+      'eat rice and vegetables for breakfast? Sometimes they eat soup.',
+      'They boil the rice. They make the soup. They cook the vegetables.',
+      'They may put vegetables in the soup. They may drink green tea',
+      'at breakfast time.',
+    ],
+    [
+      'Did you know that',
+      'sometimes people in England',
+      'eat bacon and eggs for breakfast? They fry the bacon.',
+      'They fry the eggs.',
+      'They make the toast.',
+      'They drink hot tea.',
+    ],
+    [
+      'Did you know that',
+      'sometimes people in Turkey',
+      'eat bread and cheese for breakfast? They may eat eggs and meat',
+      'with the bread and cheese. Sometimes they eat bread and nuts. They drink hot, black tea.',
+    ],
+  ];
 
-  const test: string[] =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisi vel consectetur euismod, nisi nisl aliquet nisl, eget consectetur nisl nisi vel nisl.'.split(
-      ' ',
-    );
-
-  const testType: string[] = ['New Skill', 'PT SKill'];
+  // Pages > Lines > Words
+  const bookData: String[][][] = bookRawData.map((page) => page.map((line) => line.split(' ')));
 
   return (
     <>
       <PillHeader />
       <div>
-        <h2>Pages 1 & 2</h2>
-        <p>
-          {test.map((word, i) => (
-            <span key={i} onClick={() => console.log(word)} className="cursor-pointer">
-              {word}{' '}
-            </span>
-          ))}
-        </p>
+        {bookData.map((pages, index) => (
+          <>
+            <h2>Page {index}</h2>
+            <p>
+              {pages.map((line, i) => (
+                <p>
+                  {line.map((word) => (
+                    <span key={i} onClick={() => console.log(word)} className="cursor-pointer">
+                      {word}{' '}
+                    </span>
+                  ))}
+                  <br />
+                </p>
+              ))}
+            </p>
+          </>
+        ))}
       </div>
     </>
   );
