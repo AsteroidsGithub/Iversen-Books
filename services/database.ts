62d8cab79c705ab6fe528ae7023de1a90b667fdb
Jonathan Voss <jv96@mahurangi.school.nz>
2022-08-01 20:31:55 +1200

    database: unify the project types to reflect database

--- services/database.ts
+++ services/database.ts
@@ -1,3 +1,45 @@
+import { I_BookJSON } from '@Interfaces/books';
 import { PrismaClient } from '@prisma/client';
+
+export type UserRole = 'Admin' | 'Publisher' | 'Teacher';
+
+export interface StudentProgress {
+  id: number;
+  time: number;
+  struggledWords: string[];
+  book: Book;
+  student: Student;
+}
+
+export interface Book {
+  id: number;
+  json: I_BookJSON;
+  url: string;
+  studentProgress: StudentProgress[];
+}
+
+export interface Student {
+  id: number;
+  firstName: string;
+  lastName: string;
+  class: Class;
+}
+
+export interface Class {
+  id: number;
+  name: string;
+  students: Student[];
+  teacher: User;
+}
+
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  permissions: UserRole[];
+  classes: Class[];
+}
 
 export default new PrismaClient();
